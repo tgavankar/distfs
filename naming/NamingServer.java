b41b650e982663168f2e851504eba45bbd9d6c72
tgavankar <tgavankar@gmail.com>
2012-03-05 23:48:50 -0500

    Broken locking

--- naming/NamingServer.java
+++ naming/NamingServer.java
@@ -3,6 +3,8 @@ package naming;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import rmi.*;
 import common.*;
@@ -33,10 +35,12 @@ import storage.*;
  */
 public class NamingServer implements Service, Registration
 {
-    FsNode fsRoot;
-    Skeleton<Service> clientSkeleton;
-    Skeleton<Registration> regisSkeleton;
-	HashMap<Command, Storage> storageList;
+	private FsNode fsRoot;
+    private Skeleton<Service> clientSkeleton;
+    private Skeleton<Registration> regisSkeleton;
+	private HashMap<Command, Storage> storageList;
+	private ReadWriteLock lock;
+	
     
     private class FsNode {
     	HashMap<String, FsNode> children;
@@ -103,7 +107,7 @@ public class NamingServer implements Service, Registration
     	clientSkeleton = new Skeleton<Service>(Service.class, this, new InetSocketAddress(NamingStubs.SERVICE_PORT));
     	regisSkeleton = new Skeleton<Registration>(Registration.class, this, new InetSocketAddress(NamingStubs.REGISTRATION_PORT));
     	storageList = new HashMap<Command, Storage>();
-    
+    	lock = new ReadWriteLock();
     }
 
     /** Starts the naming server.
@@ -153,6 +157,113 @@ public class NamingServer implements Service, Registration
     {
     }
 
+    public class ReadWriteLock{
+
+    	  private Map<Thread, Integer> readingThreads =
+    	       new HashMap<Thread, Integer>();
+
+    	   private int writeAccesses    = 0;
+    	   private int writeRequests    = 0;
+    	   private Thread writingThread = null;
+
+
+    	  public synchronized void lockRead() throws InterruptedException{
+    	    Thread callingThread = Thread.currentThread();
+    	    while(! canGrantReadAccess(callingThread)){
+    	      wait();
+    	    }
+
+    	    readingThreads.put(callingThread,
+    	     (getReadAccessCount(callingThread) + 1));
+    	  }
+
+    	  private boolean canGrantReadAccess(Thread callingThread){
+    	    if( isWriter(callingThread) ) return true;
+    	    if( hasWriter()             ) return false;
+    	    if( isReader(callingThread) ) return true;
+    	    if( hasWriteRequests()      ) return false;
+    	    return true;
+    	  }
+
+
+    	  public synchronized void unlockRead(){
+    	    Thread callingThread = Thread.currentThread();
+    	    if(!isReader(callingThread)){
+    	      throw new IllegalMonitorStateException("Calling Thread does not" +
+    	        " hold a read lock on this ReadWriteLock");
+    	    }
+    	    int accessCount = getReadAccessCount(callingThread);
+    	    if(accessCount == 1){ readingThreads.remove(callingThread); }
+    	    else { readingThreads.put(callingThread, (accessCount -1)); }
+    	    notifyAll();
+    	  }
+
+    	  public synchronized void lockWrite() throws InterruptedException{
+    	    writeRequests++;
+    	    Thread callingThread = Thread.currentThread();
+    	    while(! canGrantWriteAccess(callingThread)){
+    	      wait();
+    	    }
+    	    writeRequests--;
+    	    writeAccesses++;
+    	    writingThread = callingThread;
+    	  }
+
+    	  public synchronized void unlockWrite() throws InterruptedException{
+    	    if(!isWriter(Thread.currentThread())){
+    	      throw new IllegalMonitorStateException("Calling Thread does not" +
+    	        " hold the write lock on this ReadWriteLock");
+    	    }
+    	    writeAccesses--;
+    	    if(writeAccesses == 0){
+    	      writingThread = null;
+    	    }
+    	    notifyAll();
+    	  }
+
+    	  private boolean canGrantWriteAccess(Thread callingThread){
+    	    if(isOnlyReader(callingThread))    return true;
+    	    if(hasReaders())                   return false;
+    	    if(writingThread == null)          return true;
+    	    if(!isWriter(callingThread))       return false;
+    	    return true;
+    	  }
+
+
+    	  private int getReadAccessCount(Thread callingThread){
+    	    Integer accessCount = readingThreads.get(callingThread);
+    	    if(accessCount == null) return 0;
+    	    return accessCount.intValue();
+    	  }
+
+
+    	  private boolean hasReaders(){
+    	    return readingThreads.size() > 0;
+    	  }
+
+    	  private boolean isReader(Thread callingThread){
+    	    return readingThreads.get(callingThread) != null;
+    	  }
+
+    	  private boolean isOnlyReader(Thread callingThread){
+    	    return readingThreads.size() == 1 &&
+    	           readingThreads.get(callingThread) != null;
+    	  }
+
+    	  private boolean hasWriter(){
+    	    return writingThread != null;
+    	  }
+
+    	  private boolean isWriter(Thread callingThread){
+    	    return writingThread == callingThread;
+    	  }
+
+    	  private boolean hasWriteRequests(){
+    	      return this.writeRequests > 0;
+    	  }
+
+    	}
+    
     // The following public methods are documented in Service.java.
     @Override
     public void lock(Path path, boolean exclusive) throws FileNotFoundException
@@ -161,6 +272,23 @@ public class NamingServer implements Service, Registration
         	throw new NullPointerException("The path given was null.");
         if(!path.toFile(null).exists())
         	throw new FileNotFoundException("The path does not point to a file.");
+        
+        if(exclusive) {
+        	try {
+				lock.lockWrite();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+
+			}
+        }
+        else {
+        	try {
+				lock.lockRead();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+
+			}
+        }
     }
 
     @Override
@@ -170,6 +298,18 @@ public class NamingServer implements Service, Registration
         	throw new NullPointerException("The path given was null.");
     	if(!path.toFile(null).exists())
          	throw new IllegalArgumentException("The path does not point to a file.");
+    	
+        if(exclusive) {
+        	try {
+				lock.unlockWrite();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+			}
+        }
+        else {
+				lock.unlockRead();
+        }
+
     }
 
     @Override
